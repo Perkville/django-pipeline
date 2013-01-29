fb653d8a91603e26b3be77cfe2e89fb17a8c1b2a
Ryan Showalter <ryan@bixly.com>
2013-01-29 13:08:27 -0800

    Revert "Always use mimetype text/javascript due to IE8 issues"
    This reverts commit fab9d8fe5a3b14a9ac338f5481ba2c7d0fd0ca40.

--- pipeline/templatetags/compressed.py
+++ pipeline/templatetags/compressed.py
@@ -75,7 +75,7 @@ class CompressedJSNode(template.Node):
         template_name = package.template_name or "pipeline/js.html"
         context = package.extra_context
         context.update({
-            'type': 'text/javascript',
+            'type': guess_type(path, 'text/javascript'),
             'url': staticfiles_storage.url(path)
         })
         return render_to_string(template_name, context)
